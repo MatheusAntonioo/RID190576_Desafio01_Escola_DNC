d19710d07363f71d58959de75adb32da63bb9a6b
Matheus Antonio <dev.matheus.antonio@outlook.com>
2024-11-07 01:44:52 -0300

    add link e formatando

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const number = document.querySelectorAll(".infoTitle");
-const targetFix = document.querySelector("#targetFix")
+const targetFix = document.querySelector("#targetFix");
 
 // movimentando os números em informações
 function showNumbers() {
   number.forEach((counter) => {
-    function upData(){
-      const target = Number(counter.getAttribute("data-target"))
-      const count = Number(counter.innerText)
-      const inc = target / 40
+    function upData() {
+      const target = Number(counter.getAttribute("data-target"));
+      const count = Number(counter.innerText);
+      const inc = target / 40;
 
-      if(count < target){
-        counter.innerText = Math.floor(inc + count)
-        setTimeout(upData, 1)
+      if (count < target) {
+        counter.innerText = Math.floor(inc + count);
+        setTimeout(upData, 1);
       } else {
-        counter.innerText = target
-        targetFix.textContent = "2,000,000"
+        counter.innerText = target;
+        targetFix.textContent = "2,000,000";
       }
     }
-    upData()
-  })
+    upData();
+  });
 }
-showNumbers()
+showNumbers();
